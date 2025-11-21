5a217fda3691ec564c72d2c9af336fa6b133a082
Facu2701 <130311839+Facu2701@users.noreply.github.com>
2025-11-21 19:05:54 +0100

    Improve input prompts in TP INTEGRADOR.py
    Refactor user prompts for clarity and consistency.

--- Tp Integrador/TP INTEGRADOR.py
+++ Tp Integrador/TP INTEGRADOR.py
@@ -115,7 +115,7 @@ def validar_palabra(entrada):
     
     return True
 
-# Opcion 1 (Mejorar continente, poner un listado y que solo elijan con un numero, asi evitamos errores)
+# Opcion 1 
 def agregar_pais():
   while True:
    print("\n<-----Agregar nuevo pais----->")
@@ -179,7 +179,7 @@ def actualizar_datos():
   
   while True:
     print ("\n<-----Actualizar datos----->")
-    pais = input("Ingrese el nombre del pais (o 'salir' para volver al menu principal)").lower().strip()
+    pais = input("Ingrese el nombre del pais (o 'salir' para volver al menu principal): ").lower().strip()
     
     if pais == "salir":
       print("Volviendo al menu principal...")
